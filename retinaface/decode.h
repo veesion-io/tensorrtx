3240e28fe2ca0b5430ba9091a477a02a6124af56
Damien Menigaux <damien.menigaux@polytechnique.edu>
2022-05-09 15:24:28 +0200

    Adapt to Veesion situation

--- retinaface/decode.h
+++ retinaface/decode.h
@@ -13,8 +13,8 @@ namespace decodeplugin
         float class_confidence;
         float landmark[10];
     };
-    static const int INPUT_H = 480;
-    static const int INPUT_W = 640;
+    static const int INPUT_H = 576;
+    static const int INPUT_W = 704;
 }
 
 namespace nvinfer1
